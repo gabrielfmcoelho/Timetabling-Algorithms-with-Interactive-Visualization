9e492fa130e87d6c65ad67ab7e1a6896b2e60812
arthurmousinho <arthursilvamo296@gmail.com>
2024-07-04 14:03:53 -0300

    feat: gitignore file

--- local-search-algorithm/src/local_search.py
+++ local-search-algorithm/src/local_search.py
@@ -4,11 +4,6 @@ from typing import List, Dict
 
 from evaluate_solution import evaluate_solution
 from generate_initial_solution import generate_initial_solution
-
-from copy import deepcopy
-from typing import List, Dict
-import random
-
 from MetricsEvaluator import metrics_evaluator
 
 def local_search(
@@ -20,12 +15,10 @@ def local_search(
     days: List[Dict], 
     max_iterations: int = 1000
 ) -> List[Dict]:
-
-    def get_available_rooms(day_id: int, time_id: int) -> List[Dict]:
-        return [room for room in rooms if day_id in room['days'] and time_id in room['times']]
-
+    
     best_solution = generate_initial_solution(classes, teachers, rooms, times, subjects, days)
     best_score = evaluate_solution(best_solution)
+    metrics_evaluator.update_metrics(best_solution, best_score)
 
     for _ in range(max_iterations):
         metrics_evaluator.start_iteration_timer()
@@ -35,24 +28,27 @@ def local_search(
         for class_info in candidate_solution:
             for day_schedule in class_info['schedule']:
                 day_id = next(day['id'] for day in days if day['name'] == day_schedule['day'])
-                for idx, period in enumerate(day_schedule['periods']):
-                    available_rooms = get_available_rooms(day_id, idx + 1)
+                for idx, period in enumerate(day_schedule['periods'], start=1):
+                    available_rooms = get_available_rooms(rooms, day_id, idx)
                     if available_rooms:
                         random_room = random.choice(available_rooms)
                         period['room'] = random_room['name']
                     else:
                         period['room'] = "No room available"
 
-        candidate_score = evaluate_solution(candidate_solution)  
+        candidate_score = evaluate_solution(candidate_solution)
 
         if candidate_score < best_score:
             best_solution = deepcopy(candidate_solution)
             best_score = candidate_score
-
             metrics_evaluator.update_metrics(best_solution, best_score)
-            metrics_evaluator.stop_iteration_timer()
-
+        
         metrics_evaluator.update_best_metric(candidate_solution, candidate_score)
-        metrics_evaluator.stop_timer()
+        metrics_evaluator.stop_iteration_timer()
+
+    metrics_evaluator.stop_timer()
 
     return best_solution, metrics_evaluator
+
+def get_available_rooms(rooms: List[Dict], day_id: int, time_id: int) -> List[Dict]:
+    return [room for room in rooms if day_id in room['days'] and time_id in room['times']]
